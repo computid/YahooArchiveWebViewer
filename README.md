eab2b8371b32ed40c6d7726924c9e177f22f9d82
Adam <adam@craywiki.co.uk>
2023-01-19 16:37:47 +0000

    Fixed a lot of formatting issues and added attachment support

---
+++ README.md
@@ -0,0 +1,8 @@
+# Yahoo Groups Archive Renderer
+An application designed to render the archive data from the Yahoo Groups Archive Processor into a web page.
+
+## Getting Started
+
+### Prerequisites
+
+A processed Yahoo Groups archive containing a pg database with a compatible structure
---
+++ routes/index.js
@@ -0,0 +1,83 @@
+var express = require('express');
+require( 'express-async-errors');
+var router = express.Router();
+const pg = require("pg");
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  const cs = 'postgres://postgres:postgres@localhost:5432/messages';
+
+  const client = new pg.Client(cs);
+  client.connect();
+
+  const sql = 'select * from public.topics order by topicid;';
+
+  let tableView = "<table><tr><th>Topic ID</th><th>Subject</th></tr>";
+
+  try {
+    const res = await client.query(sql)
+    if (res.rows.length > 0) {
+      for(let i = 0; i < res.rows.length; i++) {
+        tableView += "<tr><td>"+res.rows[i].topicid+"</td><td><a href=\"messages?topicid="+res.rows[i].topicid+"\">"+res.rows[i].subject+"</a></td></tr>";
+      }
+    } else {
+      console.log("No valid topics found.");
+    }
+  }catch (err) {
+    console.log(err.stack)
+    console.log("error");
+    console.log(err);
+  }
+  res.render('index', { title: 'Minari Yahoo Groups Archive Viewer V1.0',
+    table: tableView
+  });
+
+});
+
+router.get('/messages', async function(req, res, next) {
+
+  let topicID = req.query.topicid;
+  const cs = 'postgres://postgres:postgres@localhost:5432/messages';
+
+  const client = new pg.Client(cs);
+  client.connect();
+
+  const sql = 'select * from messages m left join topics t on t.topicid =m.topicid where m.topicid = $1 order by m.msgid ;';
+    const values = [topicID];
+
+  let tableView = "<table border='1'><th colspan='4'>Messages</th></tr>";
+
+  try {
+    const res = await client.query(sql, values);
+    if (res.rows.length > 0) {
+      for(let i = 0; i < res.rows.length; i++) {
+        let attachmentlinks = "<table>";
+        const attachmentsQuery = 'select * from attachments a where messageid = $1;';
+        const attachmentsQueryValues = [res.rows[i].msgid];
+        const attachmentsReturn = await client.query(attachmentsQuery, attachmentsQueryValues);
+        if (attachmentsReturn.rows.length > 0) {
+          for(let i = 0; i < attachmentsReturn.rows.length; i++) {
+            attachmentlinks += "<tr><td><a href=\"/attachments/"+attachmentsReturn.rows[i].attachmentinternalname+"\">"+attachmentsReturn.rows[i].attachmentfilename+"</a></td></tr>";
+          }
+        }
+        attachmentlinks += "</table>";
+
+        const dateObject = new Date(res.rows[i].postdate * 1000)
+        const humanDateFormat = dateObject.toString()
+        tableView += "<tr><td>"+humanDateFormat+"</td><td>"+res.rows[i].subject+"</td><td>"+res.rows[i].messagefrom+"</td><td>Attachments</td></tr><tr><td colspan='3'>"+res.rows[i].rawemail+"</td><td>"+attachmentlinks+"</td></tr>";
+      }
+    } else {
+      console.log("No valid topics found.");
+    }
+  }catch (err) {
+    console.log(err.stack)
+    console.log("error");
+    console.log(err);
+  }
+  res.render('index', { title: 'Minari Yahoo Groups Archive Viewer',
+    table: tableView
+  });
+
+});
+
+module.exports = router;
